5ad81667c823df6c446a436a302749a0587d03cd
Josue <michaelesacol@gmail.com>
2021-11-17 07:20:30 -0500

    proyecto plan de apoyo, primer commit

--- archivos/script.js
+++ archivos/script.js
@@ -3,34 +3,42 @@ let bloqueActivo;
 const botonMenu = document.querySelector('.tongle');
 const botonInstrucciones = document.querySelector('#boton-instrucciones');
 
-info_metas.addEventListener('click',(e)=>{
+if(info_metas){
 
-    if (bloqueActivo){
-        bloqueActivo.classList.remove('active');
-    }
-    if(e.target.classList.contains('info')){
-        let pActivo = e.target.parentNode.parentNode.childNodes[3]
-        bloqueActivo = pActivo;
-        pActivo.classList.add('active');
+    info_metas.addEventListener('click',(e)=>{
+    
+        if (bloqueActivo){
+            bloqueActivo.classList.remove('active');
+        }
+        if(e.target.classList.contains('info')){
+            let pActivo = e.target.parentNode.parentNode.childNodes[3]
+            bloqueActivo = pActivo;
+            pActivo.classList.add('active');
+    
+        }
+    })
 
-    }
-})
+    window.addEventListener('click',(e)=>{
+        if (bloqueActivo && !e.target.classList.contains('info')){
+            bloqueActivo.classList.remove('active');
+        }
+    })
+}
 
-botonMenu.addEventListener('click',(e)=>{
-    botonMenu.classList.toggle('active');
-    if(botonMenu.classList.contains('active')){
-        document.querySelector('nav').classList.add('active')
-    }else{
-        document.querySelector('nav').classList.remove('active')
-    }
-})
+if(botonMenu){
 
-window.addEventListener('click',(e)=>{
-    if (bloqueActivo && !e.target.classList.contains('info')){
-        bloqueActivo.classList.remove('active');
-    }
-})
+    botonMenu.addEventListener('click',(e)=>{
+        botonMenu.classList.toggle('active');
+        if(botonMenu.classList.contains('active')){
+            document.querySelector('nav').classList.add('active')
+        }else{
+            document.querySelector('nav').classList.remove('active')
+        }
+    })
+}
 
-botonInstrucciones.addEventListener('click',(e) =>{
-    document.querySelector('.instrucciones').classList.toggle('active')
-})
+if(botonInstrucciones){
+    botonInstrucciones.addEventListener('click',(e) =>{
+        document.querySelector('.instrucciones').classList.toggle('active')
+    })
+}
